071561c8f1bc36b8e00a403a0d2d9ba8b838e8f9
Oluwasegun Olujemibola <olujemibolasegun@gmail.com>
2022-11-22 02:01:41 +0100

    Implemented a working book_search module

--- barcode_reader/book_search.py
+++ barcode_reader/book_search.py
@@ -1,12 +1,37 @@
 import requests
 
 
-def fetch_book_details(isbn):
-    url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(isbn)
+def fetch_book_details(isbn:int):
+    """
+    fetch_book_details function searches for book information by ISBN.
+    It uses the request module and the openlibrary API.
 
+    Parameters
+    ----------
+    isbn : int
+
+    Returns
+    -------
+    data : dict
+
+    """
+    status = None
+    url = f'https://openlibrary.org/isbn/{isbn}.json'
+    isbn_length = len(str(isbn))
+    if isbn_length != 10 and isbn_length != 13:
+        raise ValueError('ISBN length must be 10 or 13 characters')
     response = requests.get(url)
-    print(response.status_code)
-    print(response.text)
+    try:
+        data = response.json()
+    except Exception as e:
+        status = 'Failed'
+        if response.status_code == 404:
+            print('The book you\'re looking for cannot be found')
+        else:
+            print(e)
+    else:
+        status = 'Success'
+        return data
+    finally:
+        print(status)
 
-
-fetch_book_details(9780781412148)
